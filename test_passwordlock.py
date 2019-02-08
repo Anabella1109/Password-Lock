0a9bec17a609eddc6cd12363f7135995df5c3a71
TUYISENGE Anabella <bellaxbx1109@gmail.com>
2019-02-08 18:32:12 +0200

    fix error in test file

--- test_passwordlock.py
+++ test_passwordlock.py
@@ -1,11 +1,11 @@
-# import unittest
-# from passwordlock import User
-# from passwordlock_1 import Credentials
+import unittest
+from passwordlock import User
+from passwordlock_1 import Credentials
 
-# class TestUser(unittest,TestCase):
-#   '''
-#     Test class that defines test cases for the User class behaviours.
+class TestUser(unittest.TestCase):
+  '''
+    Test class that defines test cases for the User class behaviours.
 
-#     Args:
-#         unittest.TestCase: TestCase class that helps in creating test cases
-#     '''
+    Args:
+        unittest.TestCase: TestCase class that helps in creating test cases
+    '''
