f684f5cea7f64748502849d8cf8db9de8e50d7c2
TUYISENGE Anabella <bellaxbx1109@gmail.com>
2019-02-08 18:28:16 +0200

    create class that generates instances of user

---
+++ passwordlock.py
@@ -0,0 +1,22 @@
+class User:
+    '''
+    Class that generates new instances of user
+    '''
+    user_list=[]
+    def __init__(self,names,username,email,password):
+
+      '''
+      __init__ method that helps define the properties of objects
+
+      Args:
+          names:New user full name
+          username:New user's username
+          email:New user email
+          password: New user password
+
+      '''
+      self.names=names
+      self.username=username
+      self.email=email
+      self.password=password
+
---
+++ passwordlock_1.py
@@ -0,0 +1,19 @@
+# class Credentials:
+#      '''
+#      Class that  generates new instances for acount credentials
+#      '''
+#      credentials_list=[]
+#      def __init__(self,account_name,username,password):
+
+#         '''
+#         __init__ method that helps us define properties for our objects.
+
+#         Args:
+#             account_name: New account name
+#             username : New account username
+#             password: New account password
+#         '''
+
+#         self.account_name:account_name
+#         self.username:username
+#         self.password:password
