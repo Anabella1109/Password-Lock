f567a29ae83078aab472603e75cf3ddeeee29fff
TUYISENGE Anabella <bellaxbx1109@gmail.com>
2019-02-08 18:38:28 +0200

    create first test

--- test_passwordlock.py
+++ test_passwordlock.py
@@ -8,4 +8,10 @@ class TestUser(unittest.TestCase):
 
     Args:
         unittest.TestCase: TestCase class that helps in creating test cases
-    '''
+  '''
+
+  def setUp(self):
+        '''
+        Set up method to run before each test cases.
+        '''
+        self.new_user = User("James Kaki","Muriuki","james@ms.com","1234yruty") # create contact object
