1b6055d7bf8d6c055fb48940ed4196fe88d7de00
TUYISENGE Anabella <bellaxbx1109@gmail.com>
2019-02-11 09:15:55 +0200

    add test and function to delete user

--- test_passwordlock.py
+++ test_passwordlock.py
@@ -40,6 +40,17 @@ class TestUser(unittest.TestCase):
             '''
             User.user_list = []
 
+  def test_delete_user(self):
+            '''
+            test_delete_user to test if we can remove a user from our user list
+            '''
+            self.new_user.save_user()
+            test_user = User("Test","user","test@user.com","kwekwe") 
+            test_user.save_user()
+
+            self.new_user.delete_user()
+            self.assertEqual(len(User.user_list),1)
+
 
 if __name__ == '__main__':
     unittest.main()
